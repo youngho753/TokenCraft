dbe0203e7dfef1d86d1e0e601406a698016379b4
youngho753 <45281293+youngho753@users.noreply.github.com>
2024-01-23 22:31:33 +0900

    토큰,생산 엑셀파일 기본데이터 생성 -> Json변환필요

--- Assets/@Scripts/Data/Data.Contents.cs
+++ Assets/@Scripts/Data/Data.Contents.cs
@@ -31,30 +31,63 @@ namespace Data
     }
     #endregion
     
-    #region TokenData
+    #region ProductData
     [Serializable]
-    public class TokenData
+    public class ProductData
     {
         public int DataId;
-        public string PrefabId;
-        public string KoreanName;
-        public string EnglishName;
-        public string Value;
-        public string Icon;
+        public int MakerId;
+        public List<int> InputList;
+        public int MakeId;
     }
 
     [Serializable]
-    public class TokenDataLoader : ILoader<int, TokenData>
+    public class ProductLoader : ILoader<int, ProductData>
     {
-        public List<TokenData> tokens = new List<TokenData>();
-        public Dictionary<int, TokenData> MakeDict()
+        public List<ProductData> products = new List<ProductData>();
+        public Dictionary<int, ProductData> MakeDict()
         {
-            Dictionary<int, TokenData> dict = new Dictionary<int, TokenData>();
-            foreach (TokenData token in tokens)
-                dict.Add(token.DataId, token);
+            Dictionary<int, ProductData> dict = new Dictionary<int, ProductData>();
+            foreach (ProductData product in products)
+                dict.Add(product.DataId, product);
             return dict;
         }
     }
+    #endregion
+
+    #region ProductOutputData
+
+    public class ProductOutputTableData
+    {
+        public int MakeId;
+        public List<ProductOutputRateData> ProductOutputRateTable = new List<ProductOutputRateData>();
+    }
+    
+    [Serializable]
+    public class ProductOutputLoader : ILoader<int, ProductOutputTableData>
+    {
+        public List<ProductOutputTableData> ProductTable = new List<ProductOutputTableData>();
+        public Dictionary<int, ProductOutputTableData> MakeDict()
+        {
+            Dictionary<int, ProductOutputTableData> dict = new Dictionary<int, ProductOutputTableData>();
+            foreach (ProductOutputTableData product in ProductTable)
+                dict.Add(product.MakeId, product);
+            return dict;
+        }
+    }
+    
+    #endregion
+
+    #region ProductOutputRateData
+
+    public class ProductOutputRateData
+    {
+        public List<int> OutputList;
+        public float Rate;
+        public bool IsBaseItem;
+    }
+    
+
     #endregion
 
     #region MaterialtData
